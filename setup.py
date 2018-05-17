7cfee5562b8a5d3a6eef4efaab386b9b3ae96420
Brian Kopp <briankopp.usa@gmail.com>
2018-05-17 13:11:05 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pandas-io',
+  packages = ['pandas-io'], # this must be the same as the name above
+  version = '0.0.1',
+  description = 'Blazing fast pandas i/o library',
+  author = 'Brian Kopp',
+  author_email = 'briankopp.usa@gmail.com',
+  url = 'https://github.com/BrianKopp/pandas-io.git',
+  download_url = 'https://github.com/BrianKopp/pandas-io/archive/0.0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
